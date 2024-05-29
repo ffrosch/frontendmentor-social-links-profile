0a804a18baf7f5450a11d35192360650dbbdeead
Florian Frosch <22937059+ffrosch@users.noreply.github.com>
2024-05-29 12:00:11 +0200

    chore: add config option for github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/frontendmentor-social-links-profile/',
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
